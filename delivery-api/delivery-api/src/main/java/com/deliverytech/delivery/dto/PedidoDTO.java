00cc7777f501008ce07a4d85b98198b0899a282e
Danylo Miranda <danylo.s.miranda@gmail.com>
2025-10-21 22:26:25 -0300

    pedidoDTO

--- delivery-api/delivery-api/src/main/java/com/deliverytech/delivery/dto/PedidoDTO.java
+++ delivery-api/delivery-api/src/main/java/com/deliverytech/delivery/dto/PedidoDTO.java
@@ -1,5 +1,25 @@
 package com.deliverytech.delivery.dto;
 
-public class PedidoDTO {
+import com.deliverytech.delivery.entity.StatusPedido;
+import com.deliverytech.delivery.entity.Cliente;
+import com.deliverytech.delivery.entity.Restaurant;
+import com.deliverytech.delivery.entity.ItemPedido;
 
+import lombok.Data;
+import java.time.OffsetDateTime;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class PedidoDTO {
+    private Long id;
+    private OffsetDateTime dataPedido;
+    private String enderecoEntrega;
+    private BigDecimal subtotal;
+    private BigDecimal taxaEntrega;
+    private BigDecimal valorTotal;
+    private StatusPedido status;
+    private Cliente cliente;
+    private Restaurant restaurante;
+    private List<ItemPedido> itens;
 }
--- delivery-api/delivery-api/src/main/java/com/deliverytech/delivery/repository/IPedidoRepository.java
+++ delivery-api/delivery-api/src/main/java/com/deliverytech/delivery/repository/IPedidoRepository.java
@@ -1,5 +1,12 @@
 package com.deliverytech.delivery.repository;
 
-public interface IPedidoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.deliverytech.delivery.entity.Pedido;
+
+@Repository
+public interface IPedidoRepository extends JpaRepository<Pedido, Long> {
+    public Pedido findByNome(String nome);
+    public boolean existsByNome(String nome);
 }
